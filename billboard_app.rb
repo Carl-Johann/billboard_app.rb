228cbb59cd46d05192de11a8d6bcc476e9cd2800
Carl-Johan Beurling <carljohan.beurling@gmail.com>
2016-08-22 17:50:33 +0200

    printing 'top 100' in 'speed' font. Also printing the full official top 100 billboard list

--- billboard_app.rb
+++ billboard_app.rb
@@ -1,4 +1,21 @@
 require 'date'
+require 'artii'
+require 'udacicharts'
+require 'pp'
+a = Artii::Base.new :font => "speed"
+hot100 = BillboardCharts.new.get_list
 
 puts "*"*11 + Date.today.strftime("%a %d %b %Y") + "*"*11
+puts a.asciify('Hot 100')
+#pp hot100
+#p hot100
+def lort(list)
+	counter = 1
+	list.each do |makers, song|
+		puts "#{counter}. #{makers} - #{song}"
+		counter += 1
+	end
+end
+
+lort(hot100)
 
